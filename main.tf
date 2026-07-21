9e81f96c495ec2fc632d4efa4c73b3d46a94af31
Caue Casonato <caueroriz@gmail.com>
2026-07-20 21:20:01 -0700

    Provision grown stack via Jenkins

--- main.tf
+++ main.tf
@@ -9,23 +9,43 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
-
-  # Skip checks that only make sense against real AWS
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   s3_use_path_style           = true
 
   endpoints {
     s3 = "http://host.docker.internal:4566"
+    dynamodb = "http://host.docker.internal:4566"
   }
 }
 
+variable "project_name" {
+  description = "Base name used for all SnapCart resources"
+  type        = string
+  default     = "snapcart"
+}
+
 resource "aws_s3_bucket" "images" {
-  bucket = "snapcart-images"
+  bucket = "${var.project_name}-product-images"
+}
+
+resource "aws_dynamodb_table" "products" {
+  name         = "${var.project_name}-products"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "product_id"
+
+  attribute {
+    name = "product_id"
+    type = "S"
+  }
 }
 
 output "bucket_name" {
   value = aws_s3_bucket.images.bucket
+}
+
+output "table_name" {
+  value = aws_dynamodb_table.products.name
 }
